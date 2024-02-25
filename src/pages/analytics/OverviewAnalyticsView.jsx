5a8b5a669aaa800083fc2e98f60fe1b651646c3f
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-25 20:00:39 +0530

    la-8

--- src/pages/analytics/OverviewAnalyticsView.jsx
+++ src/pages/analytics/OverviewAnalyticsView.jsx
@@ -44,44 +44,7 @@ export default function OverviewAnalyticsView() {
       </Typography>
 
       <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
-          <AnalyticsWidgetSummary
-            title="Weekly Sales"
-            total={714000}
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AnalyticsWidgetSummary
-            title="New Users"
-            total={1352831}
-            color="info"
-            icon={
-              <img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />
-            }
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AnalyticsWidgetSummary
-            title="Item Orders"
-            total={1723315}
-            color="warning"
-            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
-          />
-        </Grid>
-
-        <Grid xs={12} sm={6} md={3}>
-          <AnalyticsWidgetSummary
-            title="Bug Reports"
-            total={234}
-            color="error"
-            icon={
-              <img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />
-            }
-          />
-        </Grid>
+     
 
 
         <Grid xs={12} md={6} lg={8}>
@@ -142,6 +105,15 @@ export default function OverviewAnalyticsView() {
             }}
           />
         </Grid>
+        <Grid xs={12} md={6} lg={8}>
+          <AnalyticsTasks title="Tasks" list={_analyticTasks} />
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <AnalyticsTrafficBySite
+            title="Traffic by Site"
+            list={_analyticTraffic}
+          />
+        </Grid>
 
         <Grid xs={12} md={6} lg={8}>
           <AnalyticsWebsiteVisits
@@ -190,16 +162,45 @@ export default function OverviewAnalyticsView() {
             list={_analyticOrderTimeline}
           />
         </Grid>
-
-        <Grid xs={12} md={6} lg={8}>
-          <AnalyticsTasks title="Tasks" list={_analyticTasks} />
-        </Grid>
-        <Grid xs={12} md={6} lg={4}>
-          <AnalyticsTrafficBySite
-            title="Traffic by Site"
-            list={_analyticTraffic}
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Weekly Sales"
+            total={714000}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
           />
         </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="New Users"
+            total={1352831}
+            color="info"
+            icon={
+              <img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />
+            }
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Item Orders"
+            total={1723315}
+            color="warning"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Bug Reports"
+            total={234}
+            color="error"
+            icon={
+              <img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />
+            }
+          />
+        </Grid>
+     
       </Grid>
     </Container>
   );
