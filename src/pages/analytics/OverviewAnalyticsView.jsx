a818eb31d4ba1b3b687da61e56d0865df34dd87d
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-28 14:59:19 +0530

    ca_6

--- src/pages/analytics/OverviewAnalyticsView.jsx
+++ src/pages/analytics/OverviewAnalyticsView.jsx
@@ -82,6 +82,7 @@ export default function OverviewAnalyticsView() {
             }
           />
         </Grid>
+        
 
 
         <Grid xs={12} md={6} lg={8}>
@@ -142,6 +143,126 @@ export default function OverviewAnalyticsView() {
             }}
           />
         </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+          <AnalyticsWebsiteVisits
+            title="Website Visits"
+            subheader="(+43%) than last year"
+            chart={{
+              labels: [
+                "01/01/2003",
+                "02/01/2003",
+                "03/01/2003",
+                "04/01/2003",
+                "05/01/2003",
+                "06/01/2003",
+                "07/01/2003",
+                "08/01/2003",
+                "09/01/2003",
+                "10/01/2003",
+                "11/01/2003",
+              ],
+              series: [
+                {
+                  name: "Team A",
+                  type: "column",
+                  fill: "solid",
+                  data: [23, 11, 22, 27, 13, 22, 37, 21, 44, 22, 30],
+                },
+                {
+                  name: "Team B",
+                  type: "area",
+                  fill: "gradient",
+                  data: [44, 55, 41, 67, 22, 43, 21, 41, 56, 27, 43],
+                },
+                {
+                  name: "Team C",
+                  type: "line",
+                  fill: "solid",
+                  data: [30, 25, 36, 30, 45, 35, 64, 52, 59, 36, 39],
+                },
+              ],
+            }}
+          />
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <AnalyticsOrderTimeline
+            title="Order Timeline"
+            list={_analyticOrderTimeline}
+          />
+        </Grid>
+
+        <Grid xs={12} md={6} lg={8}>
+          <AnalyticsTasks title="Tasks" list={_analyticTasks} />
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <AnalyticsTrafficBySite
+            title="Traffic by Site"
+            list={_analyticTraffic}
+          />
+        </Grid>
+        <Grid xs={12} md={6} lg={8}>
+          <AnalyticsNews title="News" list={_analyticPosts} />
+        </Grid>
+        <Grid xs={12} md={6} lg={4}>
+          <AnalyticsCurrentSubject
+            title="Current Subject"
+            chart={{
+              categories: [
+                "English",
+                "History",
+                "Physics",
+                "Geography",
+                "Chinese",
+                "Math",
+              ],
+              series: [
+                { name: "Series 1", data: [80, 50, 30, 40, 100, 20] },
+                { name: "Series 2", data: [20, 30, 40, 80, 20, 80] },
+                { name: "Series 3", data: [44, 76, 78, 13, 43, 10] },
+              ],
+            }}
+          />
+        </Grid>
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Weekly Sales"
+            total={714000}
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_bag.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="New Users"
+            total={1352831}
+            color="info"
+            icon={
+              <img alt="icon" src="/assets/icons/glass/ic_glass_users.png" />
+            }
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Item Orders"
+            total={1723315}
+            color="warning"
+            icon={<img alt="icon" src="/assets/icons/glass/ic_glass_buy.png" />}
+          />
+        </Grid>
+
+        <Grid xs={12} sm={6} md={3}>
+          <AnalyticsWidgetSummary
+            title="Bug Reports"
+            total={234}
+            color="error"
+            icon={
+              <img alt="icon" src="/assets/icons/glass/ic_glass_message.png" />
+            }
+          />
+        </Grid>
+
 
         <Grid xs={12} md={6} lg={8}>
           <AnalyticsWebsiteVisits
