181b9eb132e0523b6afc2a8fabb6ab0e2b3bcf4e
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-29 19:14:12 +0530

    cr_15

--- src/pages/analytics/OverviewAnalyticsView.jsx
+++ src/pages/analytics/OverviewAnalyticsView.jsx
@@ -44,7 +44,7 @@ export default function OverviewAnalyticsView() {
       </Typography>
 
       <Grid container spacing={3}>
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={10} md={5}>
           <AnalyticsWidgetSummary
             title="Weekly Sales"
             total={714000}
@@ -52,7 +52,7 @@ export default function OverviewAnalyticsView() {
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={10} md={5}>
           <AnalyticsWidgetSummary
             title="New Users"
             total={1352831}
@@ -63,7 +63,7 @@ export default function OverviewAnalyticsView() {
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={10} md={5}>
           <AnalyticsWidgetSummary
             title="Item Orders"
             total={1723315}
@@ -72,7 +72,7 @@ export default function OverviewAnalyticsView() {
           />
         </Grid>
 
-        <Grid xs={12} sm={6} md={3}>
+        <Grid xs={12} sm={10} md={5}>
           <AnalyticsWidgetSummary
             title="Bug Reports"
             total={234}
@@ -84,7 +84,21 @@ export default function OverviewAnalyticsView() {
         </Grid>
 
 
-        <Grid xs={12} md={6} lg={8}>
+        <Grid xs={12} md={10} lg={8}>
+          <AnalyticsCurrentVisits
+            title="Current Visits"
+            chart={{
+              series: [
+                { label: "America", value: 4344 },
+                { label: "Asia", value: 5435 },
+                { label: "Europe", value: 1443 },
+                { label: "Africa", value: 4443 },
+              ],
+            }}
+          />
+        </Grid>
+
+        <Grid xs={12} md={10} lg={8}>
           <AnalyticsConversionRates
             title="Conversion Rates"
             subheader="(+43%) than last year"
@@ -104,22 +118,8 @@ export default function OverviewAnalyticsView() {
             }}
           />
         </Grid>
-        <Grid xs={12} md={6} lg={4}>
-          <AnalyticsCurrentVisits
-            title="Current Visits"
-            chart={{
-              series: [
-                { label: "America", value: 4344 },
-                { label: "Asia", value: 5435 },
-                { label: "Europe", value: 1443 },
-                { label: "Africa", value: 4443 },
-              ],
-            }}
-          />
-        </Grid>
-
        
-        <Grid xs={12} md={6} lg={8}>
+        <Grid xs={12} md={10} lg={8}>
           <AnalyticsNews title="News" list={_analyticPosts} />
         </Grid>
         <Grid xs={12} md={6} lg={4}>
