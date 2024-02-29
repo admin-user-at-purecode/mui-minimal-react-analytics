cffce42ee8a29cd5968d54e98d8c9067230ad51a
SoniaJ-pc <soniajaiswal@purecodemarketplace.io>
2024-02-29 18:13:32 +0530

    cr_10

--- src/pages/analytics/OverviewAnalyticsView.jsx
+++ src/pages/analytics/OverviewAnalyticsView.jsx
@@ -184,16 +184,17 @@ export default function OverviewAnalyticsView() {
             }}
           />
         </Grid>
-        <Grid xs={12} md={6} lg={4}>
+        <Grid xs={12} md={10} lg={10}>
+          <AnalyticsTasks title="Tasks" list={_analyticTasks} />
+        </Grid>
+        <Grid xs={12} md={10} lg={4}>
           <AnalyticsOrderTimeline
             title="Order Timeline"
             list={_analyticOrderTimeline}
           />
         </Grid>
 
-        <Grid xs={12} md={6} lg={8}>
-          <AnalyticsTasks title="Tasks" list={_analyticTasks} />
-        </Grid>
+       
         <Grid xs={12} md={6} lg={4}>
           <AnalyticsTrafficBySite
             title="Traffic by Site"
